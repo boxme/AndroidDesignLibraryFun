0da12958c1a2450174740be4dabd4b513b38409a
boxme <desmond.ng.yang.yi@gmail.com>
2015-07-24 16:39:33 +0800

    Used coordinatorLayout to synchronize the appearance of a snackbar with fab

--- app/src/main/java/com/desmond/androiddesignlibraryfun/MainActivity.java
+++ app/src/main/java/com/desmond/androiddesignlibraryfun/MainActivity.java
@@ -1,18 +1,40 @@
 package com.desmond.androiddesignlibraryfun;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
 
+    FloatingActionButton mFabBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final View rootLayout = findViewById(R.id.rootLayout);
+        mFabBtn = (FloatingActionButton) findViewById(R.id.fabBtn);
+        mFabBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(rootLayout, "Snackbar!", Snackbar.LENGTH_LONG)
+                        .setAction("Undo", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Log.d(TAG, "SnackBar clicked");
+                            }
+                        })
+                        .show();
+            }
+        });
     }
 
     @Override
